3c1509b311856a377c0e7daf3136777d4587c40a
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2024-11-10 19:45:02 +0300

    Add DataSource

---
+++ Database/DataSource.cs
@@ -0,0 +1,20 @@
+﻿using Npgsql;
+
+namespace LibraryApiExample.Database
+{
+    public class DataSource
+    {
+        private static readonly DataSource _instance = new DataSource();
+        public NpgsqlDataSource Source { get; private set; }
+
+        private DataSource()
+        {
+            Source = NpgsqlDataSource.Create("Host=localhost;Port=5432;Username=postgres;Password=root;Database=library");
+        }
+
+        public static DataSource Instance()
+        {
+            return _instance;
+        }
+    }
+}
